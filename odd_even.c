3248cfd4d6aaa7fb5c7a37afe460f6a976a702d5
Anmol Malhotra <anmolmalhotra267@gmail.com>
2025-08-31 23:18:11 +0530

    A C program that checks whether a user-input integer is even or odd using the modulus operator. It’s a foundational exercise in control flow and arithmetic logic.
    This beginner-friendly C program demonstrates:
    - Input handling with scanf() for integers
    - Use of the modulus operator % to determine divisibility
    - Conditional branching with if-else
    - Output formatting with printf() for clear feedback
    Functionality:
    - Prompts the user to enter an integer
    - Checks if the number is divisible by 2:
    - If a % 2 == 0 → prints "a is an even number"
    - Else → prints "a is an odd number"
    - Displays the result with context-aware messaging

---
+++ odd_even.c
@@ -0,0 +1,19 @@
+//Write a program to input an integer and check whether it is even or odd using if–else.
+
+
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("Enter a number: ");
+    scanf("%d", &a);
+    if (a % 2 == 0)
+    {
+        printf("%d is an even number\n", a);
+    }
+    else
+    {
+        printf("%d is an odd number\n", a);
+    }
+    return 0;
+}
