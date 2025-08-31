3fc09aaf6a0b6312f681c3d74124084a85786d64
Anmol Malhotra <anmolmalhotra267@gmail.com>
2025-08-31 23:24:19 +0530

    A C program that determines whether a given year is a leap year
    This version accounts for:
    - Years divisible by 4 are leap years, unless:
    - They’re divisible by 100 but not by 400
    - Example:
    - 2000 → leap year
    - 1900 → not a leap year
    - 2024 → leap year

---
+++ leapyear.c
@@ -0,0 +1,19 @@
+//Write a program to input a year and check whether it is a leap year or not using conditional statements.
+
+
+#include <stdio.h>
+int main()
+{
+    int y;
+    printf("Enter year: ");
+    scanf("%d", &y);
+    if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0))
+    {
+        printf("It is a leap year");
+    }
+    else
+    {
+        printf("It is not a leap year");
+    }
+    return 0;
+}
