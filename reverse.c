be4f7fb591e721ff3abdd0858d9f05682bbfe0de
Anmol Malhotra <anmolmalhotra267@gmail.com>
2025-09-19 17:56:51 +0530

    This C program reverses a user-input integer
    This C program takes an integer input from the user and prints its reverse. It uses a while loop to extract each digit from the original number and rebuild the reversed number step by step.

---
+++ reverse.c
@@ -0,0 +1,25 @@
+// Write a program to reverse a given number.
+
+
+
+
+#include <stdio.h>
+
+int main() 
+{
+    int num, rev = 0, digit;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while (num != 0) 
+    {
+        digit = num % 10;       // Get last digit
+        rev = rev * 10 + digit; // Append to reversed number
+        num = num / 10;         // Remove last digit
+    }
+
+    printf("Reversed number is: %d\n", rev);
+
+    return 0;
+}
