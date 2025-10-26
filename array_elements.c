d9e473f84de0f4ea983087c81444b2426d45bee2
Anmol Malhotra <anmolmalhotra267@gmail.com>
2025-10-26 22:00:48 +0530

    This C program prompts the user to input a number of integers, stores them in an array, and then prints the entered values. It uses Variable Length Arrays (VLAs) to dynamically allocate the array based on user input.
    Key Features:
    - Dynamic array allocation using VLAs (int arr[n];)
    - User input handling for both array size and elements
    - Loop-based input/output using for loops

---
+++ array_elements.c
@@ -0,0 +1,24 @@
+//Read and print elements of a one-dimensional array.
+
+
+
+
+#include<stdio.h>
+int main()
+{
+    int n, i;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d numbers to be inserted in the array: ", n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    printf("You entered: \n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
